780c8e2dd695cd996cea7dcc3d4ccea9dee8a127
francisco <francisco.marao@hotmail.com>
2019-01-31 00:18:38 -0200

    fixes

--- app/src/main/java/com/colorchains/colorchainsgl/MainActivity.java
+++ app/src/main/java/com/colorchains/colorchainsgl/MainActivity.java
@@ -7,9 +7,34 @@ package com.colorchains.colorchainsgl;
 import android.app.Activity;
 import android.graphics.PixelFormat;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.games.AchievementsClient;
+import com.google.android.gms.games.AnnotatedData;
+import com.google.android.gms.games.EventsClient;
+import com.google.android.gms.games.Games;
+import com.google.android.gms.games.LeaderboardsClient;
+import com.google.android.gms.games.Player;
+import com.google.android.gms.games.PlayersClient;
+import com.google.android.gms.games.event.Event;
+import com.google.android.gms.games.event.EventBuffer;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import static android.content.ContentValues.TAG;
 
 public class MainActivity extends Activity {
     // gameView will be the view of the game
@@ -17,21 +42,183 @@ public class MainActivity extends Activity {
     // and respond to screen touches as well
     GameView gameView;
 
+    // Client used to sign in with Google APIs
+    private GoogleSignInClient mGoogleSignInClient;
+
+    // Client variables
+    private AchievementsClient mAchievementsClient;
+    private LeaderboardsClient mLeaderboardsClient;
+    private EventsClient mEventsClient;
+    private PlayersClient mPlayersClient;
+
+    // request codes we use when invoking an external activity
+    private static final int RC_UNUSED = 5001;
+    private static final int RC_SIGN_IN = 9001;
+
+    // tag for debug logging
+    private static final String TAG = "TanC";
+
+    // playing on hard mode?
+    private boolean mHardMode = false;
+
+    // achievements and scores we're pending to push to the cloud
+    // (waiting for the user to sign in, for instance)
+    private final AccomplishmentsOutbox mOutbox = new AccomplishmentsOutbox();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Create the client used to sign in to Google services.
+        mGoogleSignInClient = GoogleSignIn.getClient(this,
+                new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN).build());
+
         // Initialize gameView and set it as the view
         gameView = new GameView(this);
         hideSystemUI();
+
+        /****************/
+        startSignInIntent();
+        /****************/
         setContentView(gameView);
+        //setContentView(R.layout.activity_main);
     }
+
+    private boolean isSignedIn() {
+        return GoogleSignIn.getLastSignedInAccount(this) != null;
+    }
+
+    private void signInSilently() {
+        Log.d(TAG, "signInSilently()");
+
+        mGoogleSignInClient.silentSignIn().addOnCompleteListener(this,
+                new OnCompleteListener<GoogleSignInAccount>() {
+                    @Override
+                    public void onComplete(@NonNull Task<GoogleSignInAccount> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "signInSilently(): success");
+                            onConnected(task.getResult());
+                        } else {
+                            Log.d(TAG, "signInSilently(): failure", task.getException());
+                            onDisconnected();
+                        }
+                    }
+                });
+    }
+
+    private void onConnected(GoogleSignInAccount googleSignInAccount) {
+        Log.d(TAG, "onConnected(): connected to Google APIs");
+
+        mAchievementsClient = Games.getAchievementsClient(this, googleSignInAccount);
+        mLeaderboardsClient = Games.getLeaderboardsClient(this, googleSignInAccount);
+        mEventsClient = Games.getEventsClient(this, googleSignInAccount);
+        mPlayersClient = Games.getPlayersClient(this, googleSignInAccount);
+
+        // Show sign-out button on main menu
+        /*mMainMenuFragment.setShowSignInButton(false);*/
+
+        // Show "you are signed in" message on win screen, with no sign in button.
+        /*mWinFragment.setShowSignInButton(false);*/
+
+        // Set the greeting appropriately on main menu
+        mPlayersClient.getCurrentPlayer()
+                .addOnCompleteListener(new OnCompleteListener<Player>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Player> task) {
+                        String displayName;
+                        if (task.isSuccessful()) {
+                            displayName = task.getResult().getDisplayName();
+                        } else {
+                            Exception e = task.getException();
+                            handleException(e, getString(R.string.players_exception));
+                            displayName = "???";
+                        }
+                        /*mMainMenuFragment.setGreeting("Hello, " + displayName);*/
+                    }
+                });
+
+
+        // if we have accomplishments to push, push them
+        if (!mOutbox.isEmpty()) {
+            pushAccomplishments();
+            Toast.makeText(this, getString(R.string.your_progress_will_be_uploaded),
+                    Toast.LENGTH_LONG).show();
+        }
+
+        loadAndPrintEvents();
+    }
+
+    private void pushAccomplishments() {
+        if (!isSignedIn()) {
+            // can't push to the cloud, try again later
+            return;
+        }
+        if (mOutbox.mPrimeAchievement) {
+            mAchievementsClient.unlock(getString(R.string.achievement_prime));
+            mOutbox.mPrimeAchievement = false;
+        }
+        if (mOutbox.mArrogantAchievement) {
+            mAchievementsClient.unlock(getString(R.string.achievement_arrogant));
+            mOutbox.mArrogantAchievement = false;
+        }
+        if (mOutbox.mHumbleAchievement) {
+            mAchievementsClient.unlock(getString(R.string.achievement_humble));
+            mOutbox.mHumbleAchievement = false;
+        }
+        if (mOutbox.mLeetAchievement) {
+            mAchievementsClient.unlock(getString(R.string.achievement_leet));
+            mOutbox.mLeetAchievement = false;
+        }
+        if (mOutbox.mBoredSteps > 0) {
+            mAchievementsClient.increment(getString(R.string.achievement_really_bored),
+                    mOutbox.mBoredSteps);
+            mAchievementsClient.increment(getString(R.string.achievement_bored),
+                    mOutbox.mBoredSteps);
+            mOutbox.mBoredSteps = 0;
+        }
+        if (mOutbox.mEasyModeScore >= 0) {
+            mLeaderboardsClient.submitScore(getString(R.string.leaderboard_easy),
+                    mOutbox.mEasyModeScore);
+            mOutbox.mEasyModeScore = -1;
+        }
+        if (mOutbox.mHardModeScore >= 0) {
+            mLeaderboardsClient.submitScore(getString(R.string.leaderboard_hard),
+                    mOutbox.mHardModeScore);
+            mOutbox.mHardModeScore = -1;
+        }
+    }
+
+    private void onDisconnected() {
+        Log.d(TAG, "onDisconnected()");
+
+        mAchievementsClient = null;
+        mLeaderboardsClient = null;
+        mPlayersClient = null;
+
+        // Show sign-in button on main menu
+        /*mMainMenuFragment.setShowSignInButton(true);*/
+
+        // Show sign-in button on win screen
+        /*mWinFragment.setShowSignInButton(true);*/
+
+        /*mMainMenuFragment.setGreeting(getString(R.string.signed_out_greeting));*/
+    }
+
+    public void startSignInIntent() {
+        startActivityForResult(mGoogleSignInClient.getSignInIntent(), RC_SIGN_IN);
+    }
+
 
     // This method executes when the player starts the game
     @Override
     protected void onResume() {
         super.onResume();
         // Tell the gameView resume method to execute
+
+        // Since the state of the signed in user can change when the activity is not active
+        // it is recommended to try and sign in silently from when the app resumes.
+        signInSilently();
+
         gameView.resume();
         hideSystemUI();
     }
@@ -43,6 +230,7 @@ public class MainActivity extends Activity {
         // Tell the gameView pause method to execute
         gameView.pause();
     }
+
     // This snippet hides the system bars.
     private void hideSystemUI() {
         // Set the IMMERSIVE flag.
@@ -55,5 +243,71 @@ public class MainActivity extends Activity {
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
                         | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);//SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+    }
+
+    private void loadAndPrintEvents() {
+
+        final MainActivity mainActivity = this;
+
+        mEventsClient.load(true)
+                .addOnSuccessListener(new OnSuccessListener<AnnotatedData<EventBuffer>>() {
+                    @Override
+                    public void onSuccess(AnnotatedData<EventBuffer> eventBufferAnnotatedData) {
+                        EventBuffer eventBuffer = eventBufferAnnotatedData.get();
+
+                        int count = 0;
+                        if (eventBuffer != null) {
+                            count = eventBuffer.getCount();
+                        }
+
+                        Log.i(TAG, "number of events: " + count);
+
+                        for (int i = 0; i < count; i++) {
+                            Event event = eventBuffer.get(i);
+                            Log.i(TAG, "event: "
+                                    + event.getName()
+                                    + " -> "
+                                    + event.getValue());
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        handleException(e, getString(R.string.achievements_exception));
+                    }
+                });
+    }
+
+    private void handleException(Exception e, String details) {
+        int status = 0;
+
+        if (e instanceof ApiException) {
+            ApiException apiException = (ApiException) e;
+            status = apiException.getStatusCode();
+        }
+
+        String message = getString(R.string.status_exception_error, details, status, e);
+
+        new AlertDialog.Builder(MainActivity.this)
+                .setMessage(message)
+                .setNeutralButton(android.R.string.ok, null)
+                .show();
+    }
+
+    private class AccomplishmentsOutbox {
+        boolean mPrimeAchievement = false;
+        boolean mHumbleAchievement = false;
+        boolean mLeetAchievement = false;
+        boolean mArrogantAchievement = false;
+        int mBoredSteps = 0;
+        int mEasyModeScore = -1;
+        int mHardModeScore = -1;
+
+        boolean isEmpty() {
+            return !mPrimeAchievement && !mHumbleAchievement && !mLeetAchievement &&
+                    !mArrogantAchievement && mBoredSteps == 0 && mEasyModeScore < 0 &&
+                    mHardModeScore < 0;
+        }
     }
 }
